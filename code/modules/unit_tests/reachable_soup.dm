KOCMODECAHTHUK/Horizon-Dream

commit bab544d0971d1d69500f390fcd0a5275092eaf8f

Canreach refactor (#93165)
## About The Pull Request
ports https://github.com/DaedalusDock/daedalusdock/pull/1144
ports https://github.com/DaedalusDock/daedalusdock/pull/1147

full credit to @Kapu1178 for the juice

instead of `reacher.CanReach(target)` we now do
`target.CanBeReachedBy(reacher)`, this allows us to give special
behavior to atoms which we want to reach, which is exactly what I need
for a feature I'm working on.
## Why It's Good For The Game
allows us to be more flexible with reachability
## Changelog
:cl:
refactor: refactored how reaching items works, report any oddities with
being unable to reach something you should be able to!
/:cl:

code/_globalvars/traits/_traits.dm
@@ -685,6 +685,7 @@ GLOBAL_LIST_INIT(traits_by_type, list(
 		"TRAIT_UNCOMPOSTABLE" = TRAIT_UNCOMPOSTABLE,
 		"TRAIT_UNIQUE_AQUARIUM_CONTENT" = TRAIT_UNIQUE_AQUARIUM_CONTENT,
 		"TRAIT_WIELDED" = TRAIT_WIELDED,
+		"TRAIT_SKIP_BASIC_REACH_CHECK" = TRAIT_SKIP_BASIC_REACH_CHECK,
 	),
 	/obj/item/ammo_casing = list(
 		"TRAIT_DART_HAS_INSERT" = TRAIT_DART_HAS_INSERT,

code/_onclick/click.dm
@@ -163,7 +163,7 @@
 			UnarmedAttack(item_atom, TRUE, modifiers)
 
 	//Standard reach turf to turf or reaching inside storage
-	if(CanReach(A,W))
+	if(A.IsReachableBy(src, W?.reach))
 		if(W)
 			W.melee_attack_chain(src, A, modifiers)
 		else
@@ -200,42 +200,79 @@
 	return FALSE
 
 /**
- * A backwards depth-limited breadth-first-search to see if the target is
- * logically "in" anything adjacent to us.
+ * Returns TRUE if a movable can "Reach" this atom. This is defined as adjacency
+ *
+ * Args:
+ * * user: The movable trying to reach us.
+ * * reacher_range: How far the reacher can reach.
+ * * depth: How deep nested inside of an atom contents stack an object can be.
+ * * direct_access: Do not override. Used for recursion.
  */
-/atom/proc/CanReach(atom/ultimate_target, obj/item/tool, view_only = FALSE)
-	var/list/direct_access = DirectAccess()
-	var/depth = 1 + (view_only ? STORAGE_VIEW_DEPTH : INVENTORY_DEPTH)
+/atom/proc/IsReachableBy(atom/movable/user, reacher_range = 1, depth = INFINITY, direct_access = user.DirectAccess())
+	SHOULD_NOT_OVERRIDE(TRUE)
 
-	var/list/closed = list()
-	var/list/checking = list(ultimate_target)
+	if(isnull(user))
+		return FALSE
 
-	while (checking.len && depth > 0)
-		var/list/next = list()
-		--depth
-
-		for(var/atom/target in checking)  // will filter out nulls
-			if(closed[target] || isarea(target))  // avoid infinity situations
-				continue
-
-			if(isturf(target) || isturf(target.loc) || (target in direct_access) || (ismovable(target) && target.flags_1 & IS_ONTOP_1) || target.loc?.atom_storage) //Directly accessible atoms
-				if(Adjacent(target) || (tool && CheckToolReach(src, target, tool.reach))) //Adjacent or reaching attacks
-					return TRUE
-
-			closed[target] = TRUE
-
-			if (!target.loc)
-				continue
-
-			if(target.loc.atom_storage)
-				next += target.loc
-
-		checking = next
-
-	if(SEND_SIGNAL(src, COMSIG_ATOM_CANREACH, ultimate_target) & COMPONENT_ALLOW_REACH)
+	if(src in direct_access)
 		return TRUE
 
-	return FALSE
+	// This is a micro-opt, if any turf ever returns false from IsContainedAtomAccessible, change this.
+	if(isturf(loc) || isturf(src))
+		if(CheckReachableAdjacency(user, reacher_range))
+			return TRUE
+
+	depth--
+
+	if(depth <= 0)
+		return FALSE
+
+	if(isnull(loc) || isarea(loc) || !loc.IsContainedAtomAccessible(src, user))
+		return FALSE
+
+	return loc.IsReachableBy(user, reacher_range, depth, direct_access)
+
+/atom/proc/CheckReachableAdjacency(atom/movable/reacher, reacher_range)
+	if(reacher.Adjacent(src))
+		return TRUE
+
+	if(isliving(reacher))
+		var/mob/living/living_reacher = reacher
+		if(living_reacher.reach_length > reacher_range)
+			reacher_range = living_reacher.reach_length
+
+	return (reacher_range > 1) && RangedReachCheck(reacher, src, reacher_range)
+
+/// Called by IsReachableBy() to check for ranged reaches.
+/proc/RangedReachCheck(atom/movable/here, atom/movable/there, reach)
+	if(!here || !there)
+		return FALSE
+
+	if(reach <= 1)
+		return FALSE
+
+	// Prevent infinite loop.
+	if(istype(here, /obj/effect/abstract/reach_checker))
+		return FALSE
+
+	var/obj/effect/abstract/reach_checker/dummy = new(get_turf(here))
+	for(var/i in 1 to reach) //Limit it to that many tries
+		var/turf/T = get_step(dummy, get_dir(dummy, there))
+		if(there.IsReachableBy(dummy))
+			. = TRUE
+			break
+
+		if(!dummy.Move(T)) //we're blocked!
+			break
+
+	qdel(dummy)
+
+/// Returns TRUE if an atom contained within our contents is reachable.
+/atom/proc/IsContainedAtomAccessible(atom/contained, atom/movable/user)
+	return TRUE
+
+/atom/movable/IsContainedAtomAccessible(atom/contained, atom/movable/user)
+	return !!atom_storage
 
 /atom/proc/DirectAccess()
 	return list(src, loc)
@@ -269,7 +306,7 @@
 			dummy.SetInvisibility(INVISIBILITY_ABSTRACT)
 			for(var/i in 1 to reach) //Limit it to that many tries
 				var/turf/T = get_step(dummy, get_dir(dummy, there))
-				if(dummy.CanReach(there))
+				if(there.IsReachableBy(dummy))
 					qdel(dummy)
 					return TRUE
 				if(!dummy.Move(T)) //we're blocked!

code/modules/antagonists/heretic/magic/flesh_surgery.dm
@@ -126,7 +126,7 @@
 /datum/action/cooldown/spell/touch/flesh_surgery/proc/register_held_organ(obj/item/organ/new_held_organ, obj/item/melee/touch_attack/hand)
 	hand.vis_contents += new_held_organ
 	held_organ = new_held_organ
-	new_held_organ.flags_1 |= IS_ONTOP_1
+	ADD_TRAIT(new_held_organ, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
 	new_held_organ.vis_flags |= VIS_INHERIT_PLANE
 	RegisterSignal(new_held_organ, COMSIG_MOVABLE_MOVED, PROC_REF(unregister_held_organ))
 	RegisterSignal(new_held_organ, COMSIG_QDELETING, PROC_REF(unregister_held_organ))
@@ -139,7 +139,7 @@
 /datum/action/cooldown/spell/touch/flesh_surgery/proc/unregister_held_organ(obj/item/organ/removed_organ)
 	LAZYREMOVE(attached_hand.vis_contents, removed_organ)
 	held_organ = null
-	removed_organ.flags_1 &= ~IS_ONTOP_1
+	REMOVE_TRAIT(removed_organ, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
 	removed_organ.vis_flags &= ~VIS_INHERIT_PLANE
 	UnregisterSignal(removed_organ, list(COMSIG_MOVABLE_MOVED, COMSIG_QDELETING))
 	// Reset item offsets

code/modules/food_and_drinks/machinery/stove_component.dm
@@ -217,7 +217,7 @@
 /datum/component/stove/proc/add_container(obj/item/new_container, mob/user)
 	var/obj/real_parent = parent
 	real_parent.vis_contents += new_container
-	new_container.flags_1 |= IS_ONTOP_1
+	ADD_TRAIT(new_container, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
 	new_container.vis_flags |= VIS_INHERIT_PLANE
 
 	container = new_container
@@ -230,7 +230,7 @@
 
 /datum/component/stove/proc/remove_container()
 	var/obj/real_parent = parent
-	container.flags_1 &= ~IS_ONTOP_1
+	REMOVE_TRAIT(container, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
 	container.vis_flags &= ~VIS_INHERIT_PLANE
 	real_parent.vis_contents -= container
 

commit 26019c927ed5d0bcee79d1e0d10c0f9f1d30c669

Fix soup pots being unreachable (#93377)
## About The Pull Request

Caused by #93165 

I'm not sure but I think this was just missing a check for
`TRAIT_SKIP_BASIC_REACH_CHECK`, because soup pots are given that trait

## Changelog

:cl: Melbert
fix: You can interact with soup pots on stoves again
/:cl:

code/_globalvars/traits/_traits.dm
@@ -680,13 +680,13 @@ GLOBAL_LIST_INIT(traits_by_type, list(
 		"TRAIT_OMNI_BAIT" = TRAIT_OMNI_BAIT,
 		"TRAIT_PLANT_WILDMUTATE" = TRAIT_PLANT_WILDMUTATE,
 		"TRAIT_POISONOUS_BAIT" = TRAIT_POISONOUS_BAIT,
-		"TRAIT_T_RAY_VISIBLE" = TRAIT_T_RAY_VISIBLE,
+		"TRAIT_SKIP_BASIC_REACH_CHECK" = TRAIT_SKIP_BASIC_REACH_CHECK,
 		"TRAIT_TRANSFORM_ACTIVE" = TRAIT_TRANSFORM_ACTIVE,
+		"TRAIT_T_RAY_VISIBLE" = TRAIT_T_RAY_VISIBLE,
 		"TRAIT_UNCATCHABLE" = TRAIT_UNCATCHABLE,
 		"TRAIT_UNCOMPOSTABLE" = TRAIT_UNCOMPOSTABLE,
 		"TRAIT_UNIQUE_AQUARIUM_CONTENT" = TRAIT_UNIQUE_AQUARIUM_CONTENT,
 		"TRAIT_WIELDED" = TRAIT_WIELDED,
-		"TRAIT_SKIP_BASIC_REACH_CHECK" = TRAIT_SKIP_BASIC_REACH_CHECK,
 	),
 	/obj/item/ammo_casing = list(
 		"TRAIT_DART_HAS_INSERT" = TRAIT_DART_HAS_INSERT,

code/_onclick/click.dm
@@ -227,7 +227,9 @@
 	if(depth <= 0)
 		return FALSE
 
-	if(isnull(loc) || isarea(loc) || !loc.IsContainedAtomAccessible(src, user))
+	if(isnull(loc) || isarea(loc))
+		return FALSE
+	if(!HAS_TRAIT(src, TRAIT_SKIP_BASIC_REACH_CHECK) && !loc.IsContainedAtomAccessible(src, user))
 		return FALSE
 
 	return loc.IsReachableBy(user, reacher_range, depth, direct_access)

code/modules/antagonists/heretic/magic/flesh_surgery.dm
@@ -126,7 +126,7 @@
 /datum/action/cooldown/spell/touch/flesh_surgery/proc/register_held_organ(obj/item/organ/new_held_organ, obj/item/melee/touch_attack/hand)
 	hand.vis_contents += new_held_organ
 	held_organ = new_held_organ
-	ADD_TRAIT(new_held_organ, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
+	ADD_TRAIT(new_held_organ, TRAIT_SKIP_BASIC_REACH_CHECK, REF(src))
 	new_held_organ.vis_flags |= VIS_INHERIT_PLANE
 	RegisterSignal(new_held_organ, COMSIG_MOVABLE_MOVED, PROC_REF(unregister_held_organ))
 	RegisterSignal(new_held_organ, COMSIG_QDELETING, PROC_REF(unregister_held_organ))
@@ -139,7 +139,7 @@
 /datum/action/cooldown/spell/touch/flesh_surgery/proc/unregister_held_organ(obj/item/organ/removed_organ)
 	LAZYREMOVE(attached_hand.vis_contents, removed_organ)
 	held_organ = null
-	REMOVE_TRAIT(removed_organ, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
+	REMOVE_TRAIT(removed_organ, TRAIT_SKIP_BASIC_REACH_CHECK, REF(src))
 	removed_organ.vis_flags &= ~VIS_INHERIT_PLANE
 	UnregisterSignal(removed_organ, list(COMSIG_MOVABLE_MOVED, COMSIG_QDELETING))
 	// Reset item offsets

code/modules/food_and_drinks/machinery/stove_component.dm
@@ -217,7 +217,7 @@
 /datum/component/stove/proc/add_container(obj/item/new_container, mob/user)
 	var/obj/real_parent = parent
 	real_parent.vis_contents += new_container
-	ADD_TRAIT(new_container, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
+	ADD_TRAIT(new_container, TRAIT_SKIP_BASIC_REACH_CHECK, REF(src))
 	new_container.vis_flags |= VIS_INHERIT_PLANE
 
 	container = new_container
@@ -230,7 +230,7 @@
 
 /datum/component/stove/proc/remove_container()
 	var/obj/real_parent = parent
-	REMOVE_TRAIT(container, TRAIT_SKIP_BASIC_REACH_CHECK, ref(src))
+	REMOVE_TRAIT(container, TRAIT_SKIP_BASIC_REACH_CHECK, REF(src))
 	container.vis_flags &= ~VIS_INHERIT_PLANE
 	real_parent.vis_contents -= container
 
@@ -274,4 +274,3 @@
 		return
 	var/obj/effect/abstract/shared_particle_holder/soup_smoke = obj_parent.add_shared_particles(particle_type, "[particle_type]_stove_[container_x]")
 	soup_smoke.particles.position = list(container_x, round(ICON_SIZE_Y * 0.66), 0)
-

code/modules/unit_tests/_unit_tests.dm
@@ -253,6 +253,7 @@
 #include "quirks.dm"
 #include "range_return.dm"
 #include "rcd.dm"
+#include "reachable_soup.dm"
 #include "reagent_container_defaults.dm"
 #include "reagent_id_typos.dm"
 #include "reagent_mob_expose.dm"

code/modules/unit_tests/reachable_soup.dm
@@ -1,0 +1,13 @@
+/datum/unit_test/reachable_soup
+
+/datum/unit_test/reachable_soup/Run()
+	var/obj/machinery/oven/range/range = EASY_ALLOCATE()
+	var/obj/item/reagent_containers/cup/soup_pot/soup = EASY_ALLOCATE()
+	var/mob/living/carbon/human/dummy = EASY_ALLOCATE()
+
+	dummy.put_in_active_hand(soup)
+	click_wrapper(dummy, range)
+	TEST_ASSERT_EQUAL(soup.loc, range, "Soup pot should have been placed on the stove.")
+
+	click_wrapper(dummy, soup)
+	TEST_ASSERT_EQUAL(soup.loc, dummy, "Soup pot should have been picked up by the dummy.")