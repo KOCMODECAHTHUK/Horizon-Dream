KOCMODECAHTHUK/Horizon-Dream

commit 706a380a937c20f6ef256f5cf790cacfcf87554d

Lets sinks mount to floors (#95062)
## About The Pull Request

That's all. 

The sprites clearly have a pedestal which is attached to the floor. They
shouldn't behave as if they are free floating wash basins.

<details><summary>example: a barber shop</summary>

<img width="614" height="818" alt="StrongDMM_ahHbCymkjR"
src="https://github.com/user-attachments/assets/aeee4090-e209-4ad7-8d1d-fef9dcbb36e2"
/>

</details>

Also runs the UpdatePaths script so it can collapse some duplicate tiles
which have been annoying me with diffs each time it's run.

## Why It's Good For The Game

More mapping flexibility for bathrooms and medical spaces.

## Changelog

:cl:
code: sinks can be mounted to floors
/:cl:

---------

Co-authored-by: SyncIt21 <110812394+SyncIt21@users.noreply.github.com>

code/datums/components/atom_mounted.dm
@@ -113,7 +113,7 @@
 	return isclosedturf(target)
 
 /// Returns an list of object types we can mount on if the turf is unmountable
-/obj/proc/get_moutable_objects()
+/obj/proc/get_mountable_objects()
 	PROTECTED_PROC(TRUE)
 	SHOULD_BE_PURE(TRUE)
 	RETURN_TYPE(/list/obj)
@@ -156,7 +156,7 @@
 		if(is_mountable_turf(target))
 			attachable_atom = target //your usual wallmount
 		else
-			var/list/obj/attachables = get_moutable_objects()
+			var/list/obj/attachables = get_mountable_objects()
 			for(var/obj/attachable in target)
 				if(is_type_in_list(attachable, attachables))
 					attachable_atom = attachable

_maps/RandomRuins/LavaRuins/lavaland_battle_site.dmm
@@ -105,10 +105,6 @@
 /obj/item/stack/sheet/sinew,
 /turf/open/misc/asteroid/basalt/lava_land_surface,
 /area/lavaland/surface/outdoors)
-"L" = (
-/obj/effect/decal/cleanable/blood/drip,
-/turf/open/misc/asteroid/basalt/lava_land_surface,
-/area/lavaland/surface/outdoors)
 "M" = (
 /obj/structure/statue/bone/skull,
 /turf/open/misc/asteroid/basalt/lava_land_surface,
@@ -253,7 +249,7 @@ I
 G
 G
 q
-L
+x
 G
 s
 g

_maps/RandomZLevels/heretic.dmm
@@ -5808,9 +5808,6 @@
 /obj/effect/spawner/random/food_or_drink/seed_rare,
 /turf/open/misc/grass/jungle/station,
 /area/awaymission/beach/heretic)
-"EH" = (
-/turf/open/floor/pod/light,
-/area/awaymission/beach/heretic)
 "EI" = (
 /turf/open/water/beach,
 /area/awaymission/caves/heretic_laboratory_clean)
@@ -42214,7 +42211,7 @@ wf
 wf
 wf
 bb
-EH
+BV
 gT
 wf
 wf

code/game/objects/structures/plaques/static_plaques.dm
@@ -9,7 +9,7 @@
 		SET_PLANE_IMPLICIT(src, FLOOR_PLANE)
 		layer = HIGH_TURF_LAYER
 
-/obj/structure/plaque/static_plaque/get_moutable_objects()
+/obj/structure/plaque/static_plaque/get_mountable_objects()
 	return list()
 
 /obj/structure/plaque/static_plaque/find_and_mount_on_atom(mark_for_late_init, late_init)

code/game/objects/structures/water_structures/sink.dm
@@ -49,16 +49,14 @@ MAPPING_DIRECTIONAL_HELPERS(/obj/structure/sink, (-14))
 	if(has_water_reclaimer)
 		new /obj/item/stock_parts/water_recycler(drop_location())
 
-/obj/structure/sink/get_turfs_to_mount_on()
-	return list(get_step(src, REVERSE_DIR(dir)))
+/obj/structure/sink/is_mountable_turf(turf/target)
+	return !isgroundlessturf(target)
 
-/obj/structure/sink/get_moutable_objects()
-	var/static/list/sink_structures = null
-	if(isnull(sink_structures))
-		sink_structures = list()
-		sink_structures += ..()
-		sink_structures += /obj/machinery/smartfridge //medbay sometimes have sinks attached to fridges
-	return sink_structures
+/obj/structure/sink/get_turfs_to_mount_on()
+	return list(get_turf(src))
+
+/obj/structure/sink/get_mountable_objects()
+	return list()
 
 /obj/structure/sink/add_context(atom/source, list/context, obj/item/held_item, mob/living/user)
 	. = NONE

code/modules/power/lighting/light.dm
@@ -757,7 +757,7 @@
 /obj/machinery/light/floor/is_mountable_turf(turf/target)
 	return !isgroundlessturf(target)
 
-/obj/machinery/light/floor/get_moutable_objects()
+/obj/machinery/light/floor/get_mountable_objects()
 	var/static/list/attachables = list(
 		/obj/structure/thermoplastic,
 		/obj/structure/lattice/catwalk,

commit 0e2f6e409e1e17ed16fc29d87c89e6127a20e680

Fixes floor mounts not attaching to plating (#95319)
## About The Pull Request
- Fixes
https://github.com/tgstation/tgstation/pull/94993#issuecomment-3797322473
- Partially addresses #95310. That is deconstructing the floor tile of
the sink won't destroy it. This behaviour was caused by #95062 which
made sinks mount to floors & not walls

## Changelog
:cl:
fix: you can pry out/deconstruct floor tiles/reinforced tiles etc
without destroying any floor lights/sinks mounted on as long as there is
plating underneath that turf
/:cl:

---------

Co-authored-by: Time-Green <7501474+Time-Green@users.noreply.github.com>

code/datums/components/atom_mounted.dm
@@ -4,12 +4,13 @@
 	var/atom/hanging_support_atom
 
 /datum/component/atom_mounted/Initialize(target_structure)
-	. = ..()
 	if(!isobj(parent) || !isatom(target_structure))
 		return COMPONENT_INCOMPATIBLE
+	. = ..()
+
 	hanging_support_atom = target_structure
 	RegisterSignal(hanging_support_atom, COMSIG_ATOM_EXAMINE, PROC_REF(on_examine))
-	if(isclosedturf(hanging_support_atom))
+	if(isturf(hanging_support_atom))
 		RegisterSignal(hanging_support_atom, COMSIG_TURF_CHANGE, PROC_REF(on_turf_changing))
 	else
 		RegisterSignal(hanging_support_atom, COMSIG_QDELETING, PROC_REF(on_structure_delete))
@@ -28,6 +29,7 @@
 	UnregisterSignal(parent, signals)
 
 /datum/component/atom_mounted/Destroy(force)
+	UnregisterSignal(hanging_support_atom, COMSIG_ATOM_EXAMINE)
 	hanging_support_atom = null
 	return ..()
 
@@ -37,25 +39,44 @@
 /datum/component/atom_mounted/proc/on_examine(datum/source, mob/user, list/examine_list)
 	SIGNAL_HANDLER
 
-	if (parent in view(user.client?.view || world.view, user))
+	if(parent in view(user.client?.view || world.view, user))
 		examine_list += span_notice("\The [hanging_support_atom] is currently supporting [span_bold("\the [parent]")]. Deconstruction or excessive damage would cause it to [span_bold("fall to the ground")].")
 
 /// When the type of turf changes, if it is changing into a floor we should drop our contents
-/datum/component/atom_mounted/proc/on_turf_changing(datum/source, path, new_baseturfs, flags, post_change_callbacks)
+/datum/component/atom_mounted/proc/on_turf_changing(turf/source, path, new_baseturfs, flags, post_change_callbacks)
 	SIGNAL_HANDLER
 
-	if(ispath(path, /turf/open))
-		drop_wallmount()
+	//if we transforming from open to open turf we can skip deconstruction under some conditions
+	if(isopenturf(source) && ispath(path, /turf/open))
+		var/reload = FALSE
+
+		//we are transforming from plating into anything that isn't space
+		if(isplatingturf(source) && !ispath(path, /turf/open/space))
+			reload = TRUE
+		//we are transforming into plating turf
+		else if(ispath(LAZYACCESS(source.baseturfs, length(source.baseturfs)), /turf/open/floor/plating))
+			reload = TRUE
+
+		if(reload)
+			var/obj/target = parent
+			qdel(src)
+			post_change_callbacks += CALLBACK(target, TYPE_PROC_REF(/obj, remount))
+			return
+
+	drop_wallmount()
 
 ///When the atom the object is mounted on is destroyed deconstruct
 /datum/component/atom_mounted/proc/on_structure_delete(datum/source, force)
 	SIGNAL_HANDLER
+	PRIVATE_PROC(TRUE)
 
 	drop_wallmount()
 
 /// If we get dragged from our wall (by a singulo for instance) we should deconstruct
 /datum/component/atom_mounted/proc/on_move(datum/source, atom/old_loc, dir, forced, list/old_locs)
 	SIGNAL_HANDLER
+	PRIVATE_PROC(TRUE)
+
 	// If we're having our lighting messed with we're likely to get dragged about
 	// That shouldn't lead to a decon
 	if(HAS_TRAIT(parent, TRAIT_LIGHTING_DEBUGGED))
@@ -65,6 +86,7 @@
 ///Called when the object is about to be shuttle rotated so we have to delete ourself and mount again later
 /datum/component/atom_mounted/proc/detach(datum/source, newT, rotation, move_mode, moving_dock)
 	SIGNAL_HANDLER
+	PRIVATE_PROC(TRUE)
 
 	qdel(src)
 
@@ -175,8 +197,8 @@
 		obj_flags |= MOUNT_ON_LATE_INITIALIZE
 	return FALSE
 
-///Used to remount an object after shuttle move
-/obj/proc/remount(datum/source, oldT)
+///Used to remount an object in special cases
+/obj/proc/remount()
 	SIGNAL_HANDLER
 	PRIVATE_PROC(TRUE)
 